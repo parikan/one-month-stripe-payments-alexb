8aa9be92ead3184d6349984e9d0f8c2435fece68
Alexandre Balay <alex77r@gmail.com>
2015-09-10 09:24:17 -0400

    Adding emailing of a purchase receipt

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -2,4 +2,11 @@ class Purchase < ActiveRecord::Base
 	def to_param
 	 uuid
 	end
+	after_create :email_purchaser
+
+	private
+
+	def email_purchaser
+	   PurchaseMailer.purchase_receipt(self).deliver
+	end
 end
